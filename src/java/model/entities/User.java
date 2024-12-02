0ef1333ffcbf85001ec105c0b3a984b98a6c029a
Antoni Llop <antoni.llop@estudiants.urv.cat>
2024-12-02 22:19:36 +0100

    Initial commit

---
+++ src/java/model/entities/User.java
@@ -0,0 +1,93 @@
+package model.entities;
+
+import java.io.Serializable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @SequenceGenerator(name = "User_Gen", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "User_Gen")
+    private Long id;
+
+    private String username; // Nombre de usuario
+    private String email;    // Correo electrónico
+    private String password; // Contraseña del usuario
+
+    private boolean isAdmin; // Indica si el usuario tiene permisos de administrador
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    // hashCode, equals y toString
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // Nota: este método no funcionará si los campos id no están configurados
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.entities.User[ id=" + id + ", username=" + username + " ]";
+    }
+}
---
+++ src/java/service/RESTapp.java
@@ -0,0 +1,8 @@
+package service;
+
+import jakarta.ws.rs.core.Application;
+
+@jakarta.ws.rs.ApplicationPath("webresources") // aqui s'ha de ficar /rest/api/v1 etc...
+public class RESTapp extends Application {
+    
+}
