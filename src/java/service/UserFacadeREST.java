e78fc8ac354ed7f1912a8d68d7be084af4d90972
Antoni Llop <tonichi003@gmail.com>
2024-12-03 12:15:11 +0100

    Primer Metodo GET USER

--- src/java/service/UserFacadeREST.java
+++ src/java/service/UserFacadeREST.java
@@ -11,6 +11,9 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import model.entities.Article;
 import model.entities.User;
@@ -33,12 +36,25 @@ public class UserFacadeREST extends AbstractFacade<User> {
     }
 
     @GET
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response getCustomers(){
-    // Agafar to 
+@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+public Response getCustomers() {
+        // Consultar users
     String query = "SELECT u FROM Usuari u";
     List<User> users = em.createQuery(query, User.class).getResultList();
+    Collection<User> retorn = new ArrayList<>();
 
-    
+    // Recorrer users
+    for (User u : users) {
+        u.setPassword(null);
+        String linkArticle = null;
+        if (u.getArticles() != null && !u.getArticles().isEmpty()) {
+            Article lastArticle = u.getArticles().get(u.getArticles().size() - 1);
+            linkArticle = "/article/" + lastArticle.getId();
+        }
+        u.setCodiArticle(linkArticle);
+        retorn.add(u);
     }
-
+    return Response.status(Response.Status.OK)
+                   .entity(retorn)
+                   .build();
+}
